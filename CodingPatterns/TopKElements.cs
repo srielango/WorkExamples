45df5291319bd1159858ac139c3dee1b9c4a5002
srielango <srielango@gmail.com>
2025-02-19 12:48:00 +0530

    Added TopKElements pattern

---
+++ CodingPatterns/TopKElements.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPatterns
+{
+    public class TopKElements
+    {
+        private ITopKElementsStrategy _topKElementsStrategy;
+
+        public TopKElements(ITopKElementsStrategy topKElementsStrategy)
+        {
+            _topKElementsStrategy = topKElementsStrategy;
+        }
+
+        public int[] GetTopKElements(int[] nums, int k)
+        {
+            return _topKElementsStrategy.TopKFrequent(nums, k);
+        }
+    }
+
+    public interface ITopKElementsStrategy
+    {
+        public int[] TopKFrequent(int[] nums, int k);
+    }
+
+    public class HashMapTopKElementsStrategy : ITopKElementsStrategy
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> ElementDectionary = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(ElementDectionary.ContainsKey(nums[i]))
+                {
+                    ElementDectionary[nums[i]] = ElementDectionary[nums[i]] + 1;
+                }
+                else
+                {
+                    ElementDectionary.Add(nums[i], 1);
+                }
+            }
+
+            var sortedDictionary = ElementDectionary.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            var result = new int[k];
+            var index = 0;
+            foreach(var pair in sortedDictionary)
+            {
+                result[index++] = pair.Key;
+                if(index >= k)
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CodingPatternsTests/TopKElementsTests.cs
@@ -0,0 +1,24 @@
+﻿using CodingPatterns;
+using FluentAssertions;
+
+namespace CodingPatternsTests
+{
+    [TestClass]
+    public class TopKElementsTests
+    {
+        public readonly TopKElements _sut;
+        public TopKElementsTests()
+        {
+            _sut = new TopKElements(new HashMapTopKElementsStrategy());
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 1, 1, 2, 2, 3 }, 2, new int[] {1,2})]
+        [DataRow(new int[] {1}, 1, new int[] { 1 })]
+        public void intArrayReturnsExpectedElements(int[] nums, int k, int[] exptected)
+        {
+            var result = _sut.GetTopKElements(nums, k);
+            result.Should().BeEquivalentTo(exptected);
+        }
+    }
+}
