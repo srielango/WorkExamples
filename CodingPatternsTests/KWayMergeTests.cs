e9a002d9f387451f137bd166b68e2cf994ca1322
srielango <srielango@gmail.com>
2025-03-13 00:56:40 +0530

    Added Knapsack coding pattern

---
+++ CodingPatternsTests/KWayMergeTests.cs
@@ -0,0 +1,38 @@
+﻿using CodingPatterns;
+using FluentAssertions;
+
+namespace CodingPatternsTests
+{
+    [TestClass]
+    public class KWayMergeTests
+    {
+        private readonly KWayMerge _sut;
+
+        public KWayMergeTests()
+        {
+            _sut = new KWayMerge();
+        }
+
+        [TestMethod]
+        [DataRow(new int[] {1,2,3,0,0,0}, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 })]
+        public void intArraysMergedToFirstArray(int[] num1, int m, int[] num2, int n, int[] expected)
+        {
+            _sut.Merge(num1, m, num2, n);
+            num1.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void intArraysMergedAndSorted()
+        {
+            var source = new int[2,3]
+            {
+                { 1, 2, 3 },
+                { 2, 4, 5 }
+            };
+
+            var expected = new int[] {1, 2, 2, 3, 4 ,5 };
+            var result = _sut.Merge(source);
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ CodingPatternsTests/KnapsackTests.cs
@@ -0,0 +1,25 @@
+﻿using CodingPatterns;
+using FluentAssertions;
+
+namespace CodingPatternsTests
+{
+    [TestClass]
+    public class KnapsackTests
+    {
+        private readonly Knapsack _sut;
+
+        public KnapsackTests()
+        {
+            _sut = new Knapsack(new NaiveStrategy());
+        }
+
+        [TestMethod]
+        [DataRow(4, new int[] { 1, 2, 3 }, new int[] { 4, 5, 1 }, 3)]
+        [DataRow(3, new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, 0)]
+        public void MaxProfit_ShouldReturnExpectedProfit(int capacity, int[] profits, int[] weights, int expected)
+        {
+            var result = _sut.GetMaxProfit(capacity, profits, weights);
+            result.Should().Be(expected);
+        }
+    }
+}
